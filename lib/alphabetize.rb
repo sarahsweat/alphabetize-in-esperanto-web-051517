57a5ba78953dd47936038ddbbac17c762eb2a2c9
Sarah Sweat <sarahsweat@gmail.com>
2017-05-08 23:46:54 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+
+  arr.sort_by { |phrase| phrase.split("").map{ |character| ESPERANTO_ALPHABET.index(character) } }
+  
 end
